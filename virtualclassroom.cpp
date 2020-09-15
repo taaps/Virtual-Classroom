50b57555f0a5215aa7763c7555ce6946fa45af69
Tapasvi <tapasvi101@gmail.com>
2020-09-14 22:47:32 -0400

    Added calendar widget and button functionalities.

--- virtualclassroom.cpp
+++ virtualclassroom.cpp
@@ -6,9 +6,63 @@ VirtualClassroom::VirtualClassroom(QWidget *parent) :
     ui(new Ui::VirtualClassroom)
 {
     ui->setupUi(this);
+    this->setStyleSheet("background-color: white");
+    ui->groupBox->setStyleSheet("background-color: lightgrey");
+    ui->pushButton_home->setStyleSheet("background-color: white");
+    ui->pushButton_class->setStyleSheet("background-color: white");
+    ui->pushButton_announcement->setStyleSheet("background-color: white");
+    ui->pushButton_calendar->setStyleSheet("background-color: white");
+    ui->pushButton_file->setStyleSheet("background-color: white");
+    ui->pushButton_chat->setStyleSheet("background-color: white");
+    ui->pushButton_setting->setStyleSheet("background-color: white");
+
+    // Hide necessary widgets
+    ui->calendarWidget->hide();
 }
 
 VirtualClassroom::~VirtualClassroom()
 {
     delete ui;
+}
+
+void VirtualClassroom::on_pushButton_calendar_clicked()
+{
+    // Show necessary widgets
+    ui->calendarWidget->show();
+}
+
+void VirtualClassroom::on_pushButton_home_clicked()
+{
+    // Hide necessary widgets
+    ui->calendarWidget->hide();
+}
+
+void VirtualClassroom::on_pushButton_class_clicked()
+{
+    // Hide necessary widgets
+    ui->calendarWidget->hide();
+}
+
+void VirtualClassroom::on_pushButton_announcement_clicked()
+{
+    // Hide necessary widgets
+    ui->calendarWidget->hide();
+}
+
+void VirtualClassroom::on_pushButton_file_clicked()
+{
+    // Hide necessary widgets
+    ui->calendarWidget->hide();
+}
+
+void VirtualClassroom::on_pushButton_chat_clicked()
+{
+    // Hide necessary widgets
+    ui->calendarWidget->hide();
+}
+
+void VirtualClassroom::on_pushButton_setting_clicked()
+{
+    // Hide necessary widgets
+    ui->calendarWidget->hide();
 }
--- virtualclassroom.h
+++ virtualclassroom.h
@@ -15,6 +15,21 @@ public:
     explicit VirtualClassroom(QWidget *parent = nullptr);
     ~VirtualClassroom();
 
+private slots:
+    void on_pushButton_calendar_clicked();
+
+    void on_pushButton_home_clicked();
+
+    void on_pushButton_class_clicked();
+
+    void on_pushButton_announcement_clicked();
+
+    void on_pushButton_file_clicked();
+
+    void on_pushButton_chat_clicked();
+
+    void on_pushButton_setting_clicked();
+
 private:
     Ui::VirtualClassroom *ui;
 };
